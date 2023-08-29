5ce8b64ab6bdb16a033f220dc0154999810d6fa0
MuhammadAkmalHamidi <132432072+MuhammadAkmalHamidi@users.noreply.github.com>
2023-08-29 17:43:59 +0700

    [fix-front-end-dependecies] (#4)
    * initial dependencies
    
    * add: front-end-dep
    
    * fix: file-client
    
    ---------
    
    Co-authored-by: LaziestCoder <andi.id.dev@gmail.com>
    Co-authored-by: LittleGarden <51283033+anditiana@users.noreply.github.com>

--- projects/client/src/routes/index.jsx
+++ projects/client/src/routes/index.jsx
@@ -1,7 +1,8 @@
 import { Route, createRoutesFromElements } from "react-router-dom";
+import HomepageView from "../views/Home";
 
-import SignInSide from "../components/tenant/login-page";
 
-const Routes = <Route path="/" element={<SignInSide />} />;
+const Routes = <Route path="/" element={<HomepageView />} />;
+
 
 export const routes = createRoutesFromElements(Routes);
