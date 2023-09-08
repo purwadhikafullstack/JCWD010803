06f73cda12e0aadbee568905f46dcd7f1d94c809
MuhammadAkmalHamidi <hamidiakmal193@gmail.com>
2023-09-08 09:54:47 +0700

    delete: delete console.log

--- projects/client/src/components/profile-setting/profile-setting-select.jsx
+++ projects/client/src/components/profile-setting/profile-setting-select.jsx
@@ -7,7 +7,6 @@ export const ProfileSettingSelect = ({choose, values}) => {
     const [click, setClick] = useState("")    
     const dataFireBase = useSelector((state) => state.firebase.value)
 	const data = useSelector((state) => state.user.value)
-	console.log(data);
 
     const clickChangePassword = () => {
 		setClick("changePassword")
