65d91228bbd5cd7676c03ce730e95ef08672a523
Juliansyah Rizky <jratrainbows@gmail.com>
2023-09-15 09:04:50 +0700

    add: login to WRP 25

--- projects/client/src/routes/index.jsx
+++ projects/client/src/routes/index.jsx
@@ -1,20 +1,24 @@
-import LandingPage from "../pages/landing-page";
-import Register from "../pages/Register";
+// import LandingPage from "../pages/landing-page";
+// import Register from "../pages/Register";
 import { Route, createRoutesFromElements } from "react-router-dom";
-import { ProfileSettingPage } from "../pages/profile-setting-page";
-import { ForgotPasswordPage } from "../pages/forgot-password";
-import { ResetPasswordPage } from "../pages/reset-password-page";
-import { LoginUserPage } from "../pages/login-page";
+import LoginTenant from "../pages/login-tenant";
+// import { ProfileSettingPage } from "../pages/profile-setting-page";
+// import { ForgotPasswordPage } from "../pages/forgot-password";
+// import { ResetPasswordPage } from "../pages/reset-password-page";
+// import { LoginUserPage } from "../pages/login-page";
+// import RegisterTenant from "../pages/register-tenant";
+
 
 
 
 const Routes = <>
-    <Route path="/" element={<LandingPage />} />
-    <Route path="/register" element={<Register />} />
+    <Route path="/" element={<LoginTenant/>} />
+    {/* <Route path="/register" element={<Register />} />
     <Route path="/login" element={<LoginUserPage />} />;
     <Route path="/profile-setting" element={<ProfileSettingPage />} />;
     <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-    <Route path="/reset-password/:Token" element={<ResetPasswordPage />} />
+    <Route path="/reset-password/:Token" element={<ResetPasswordPage />} /> */}
+
 </> 
 
 export const routes = createRoutesFromElements(Routes);
