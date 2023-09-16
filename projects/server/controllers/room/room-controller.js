c2cfd6bbff64eff587d736674dfe49a0f8062e00
MuhammadAkmalHamidi <hamidiakmal193@gmail.com>
2023-09-16 23:03:42 +0700

    fix: add room image

--- projects/server/controllers/room/room-controller.js
+++ projects/server/controllers/room/room-controller.js
@@ -1,36 +1,45 @@
 const db = require("../../models");
 const room = db.rooms;
+const roomImg = db.roomImg;
 
 module.exports = {
   addRoom: async (req, res) => {
     try {
-      const  propertyId  = req.params.id;
-      const { roomNumber, roomName, price, roomDesc } = req.body;
+      const { roomName, price, roomDesc} = req.body;
+      const propertyId = req.params.id;
+      const data = req.files
+
       const result = await room.create({
-        roomNumber,
         roomName,
         roomDesc,
         price,
         propertyId,
       });
+      const pathImg = data.map((item) => {
+        return {
+          image: item.filename,
+          roomId: result.id,
+        };
+      });
+      const addImg = await roomImg.bulkCreate(pathImg)
       res.status(200).send({
         message: "add room success",
       });
     } catch (error) {
-      res.status(500).send({
-        status: 500,
-        message: "Internal server error.",
-        error,
-      });
+      console.log(error);
+      res.status(400).send(error);
     }
   },
   updateRoom: async (req, res) => {
     try {
       const { roomName, price, roomDesc } = req.body;
-      const {id} = req.params
-      const result = await room.update({roomName,roomDesc,price}, {
-        where: { id:id },
-      });
+      const { id } = req.params;
+      const result = await room.update(
+        { roomName, roomDesc, price },
+        {
+          where: { id: id },
+        }
+      );
       res.status(200).send({
         message: "update success",
       });
@@ -48,7 +57,7 @@ module.exports = {
       const { id } = req.params;
       const result = await room.update(
         { isDelete: true },
-        { where: { id: id} }
+        { where: { id: id } }
       );
       res.status(200).send({
         message: "delete success",
--- projects/server/routers/room-router.js
+++ projects/server/routers/room-router.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { roomController } = require('../controllers/room')
 const { checkRoom } = require('../midlewares/tenant/validatorRoom')
+const { multerUpload } = require('../midlewares/multer')
 
 const router = express.Router()
 
-router.post('/:id',checkRoom,roomController.addRoom)
+router.post('/:id',multerUpload(`./public/room`, 'roomImg').array('roomImg'),checkRoom,roomController.addRoom)
 router.patch('/delete/:id', roomController.deleteRoom)
 router.patch('/:id', roomController.updateRoom)
 
