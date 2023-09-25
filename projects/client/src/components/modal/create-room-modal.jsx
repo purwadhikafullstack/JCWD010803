2c471350f15a18fb88f4e489fb0a6e8092a8e2cc
MuhammadAkmalHamidi <hamidiakmal193@gmail.com>
2023-09-25 14:22:28 +0700

    get property id by params

--- projects/client/src/components/modal/create-room-modal.jsx
+++ projects/client/src/components/modal/create-room-modal.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { ErrorMessage, Field, Form, Formik } from 'formik'
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import * as Yup from 'yup'
 
-export const CreateRoomModal = ({propertyId ,openModal, setOpenModal, setReload, reload}) => {
+export const CreateRoomModal = ({ propertyId, openModal, setOpenModal, setReload, reload }) => {
+    const params = useParams()
 
     const [file1, setFile1] = useState(null)
     const [file2, setFile2] = useState(null)
@@ -19,7 +21,7 @@ export const CreateRoomModal = ({propertyId ,openModal, setOpenModal, setReload,
     const addRoom = async (data) => {
         try {
             const formData = new FormData()
-            const {roomName, roomDesc, price} = data
+            const { roomName, roomDesc, price } = data
             formData.append("roomName", roomName)
             formData.append("roomDesc", roomDesc)
             formData.append("price", price)
@@ -27,7 +29,11 @@ export const CreateRoomModal = ({propertyId ,openModal, setOpenModal, setReload,
             formData.append("roomImg", file2)
             formData.append("roomImg", file3)
             formData.append("roomImg", file4)
-            const response = await axios.post(`http://localhost:8000/api/room/${propertyId}`, formData)
+            const response = await axios.post(`http://localhost:8000/api/room/${params.propertyId}`, formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            })
             setOpenModal(false)
             setReload(!reload)
         } catch (error) {
