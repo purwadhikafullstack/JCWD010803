847eb565034b94909e15dd1018c0e64e7927360d
gangsararyop <86463063+gangsararyop@users.noreply.github.com>
2023-10-15 22:45:25 +0700

    Update config.js

--- projects/server/src/config/config.js
+++ projects/server/src/config/config.js
@@ -1,25 +1,25 @@
 require('dotenv').config()
 
 module.exports = {
-  development: {
-    username: process.env.USERNAME_DATABASE,
-    password: process.env.PASSWORD_DATABASE,
-    database: process.env.DATABASE_NAME,
-    host: "127.0.0.1",
-    dialect: "mysql"
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  },
-  productions: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  }
+ development: {
+		username: process.env.USERNAME_DATABASE,
+		password: process.env.PASSWORD_DATABASE,
+		database: process.env.NAME_DATABASE,
+		host: "127.0.0.1",
+		dialect: "mysql",
+	},
+	test: {
+		username: process.env.USERNAME_DATABASE,
+		password: process.env.PASSWORD_DATABASE,
+		database: process.env.NAME_DATABASE,
+		host: "127.0.0.1",
+		dialect: "mysql",
+	},
+	production: {
+		username: "jcwd010803",
+		password: "jcwd010803",
+		database: "jcwd010803",
+		host: "adminer2.purwadhikabootcamp.com",
+		dialect: "mysql",
+	},
 }
