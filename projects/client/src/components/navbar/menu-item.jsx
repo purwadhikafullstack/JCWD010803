92340cbe04131e2ae520432eb217607abb4c4056
MuhammadAkmalHamidi <hamidiakmal193@gmail.com>
2023-10-18 15:50:14 +0700

    fix: menu button

--- projects/client/src/components/navbar/menu-item.jsx
+++ projects/client/src/components/navbar/menu-item.jsx
@@ -5,7 +5,7 @@ const MenuItem = ({ onClick, label }) => {
   return (
     <div
       onClick={onClick}
-      className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+      className="px-4 py-3 hover:bg-neutral-100 hover:rounded-xl font-semibold"
     >
       {label}
     </div>
